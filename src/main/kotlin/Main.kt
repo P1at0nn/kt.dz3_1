5ba79522590d42bf23593dc4e99d490ddae64ab2
platon an47 <verzunovplat0n@gmail.com>
2023-08-30 19:22:37 +0300

    commit add program

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,32 @@
+fun main(args: Array<String>) {
+
+    val Timeago: Int = 2 * 60
+    val Timeagoin = Timeago / 60
+    val Timeagoin60 = Timeago / 60 / 60
+
+
+    var agoToText = when (Timeago) {
+        60 -> println("был(а) 1 минуту назад")
+        120 -> println("был(а) 2 минуты назад")
+        21 * 60 -> println("был(а) 21 минуту назад")
+        in 60 * 22..60 * 24 -> println("был(а) $Timeagoin минуты назад")
+        in 60 * 32..60 * 34 -> println("был(а) $Timeagoin минуты назад")
+        in 60 * 42..60 * 44 -> println("был(а) $Timeagoin минуты назад")
+        in 60 * 52..60 * 54 -> println("был(а) $Timeagoin минуты назад")
+
+
+        in 60 * 60..60 * 60 * 2 - 1 -> println("был(а) $Timeagoin60 час назад")
+        in 60 * 60 * 2..60 * 60 * 5 - 1 -> println("был(а) $Timeagoin60 часа назад")
+        in 60 * 60 * 21..60 * 60 * 22 - 1 -> println("был(а) $Timeagoin60 час назад")
+        in 60 * 60 * 22..60 * 60 * 24 -> println("был(а) $Timeagoin60 часа назад")
+
+
+        in 0..60 -> println("был(а) только что ")
+        in 61..60 * 60 -> println("был(а) $Timeagoin минут назад")
+        in 60 * 60 + 1..24 * 60 * 60 -> println(" был(а)$Timeagoin60 часов назад")
+        in 24 * 60 * 60 + 1..24 * 60 * 60 * 2 -> println("был(а) вчера ")
+        in 24 * 60 * 60 * 2 + 1..24 * 60 * 60 * 3 -> println("был(а) позавчера ")
+        in 24 * 60 * 60 * 3 + 1..24 * 60 * 60 * 60 * 60 -> println("был(а) давно ")
+        else -> println("данные не коректны")
+    }
+}
